e9dcf934d70b7717f381a577c50a415adcd64e81
emmatotoro <130192726+emmatotoro@users.noreply.github.com>
2023-04-07 20:39:27 -0400

    Update main.py

--- main.py
+++ main.py
@@ -90,7 +90,7 @@ import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
 scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
-creds = ServiceAccountCredentials.from_json_keyfile_name('/Users/emmakehoe/Desktop/credentials.json', scope)
+creds = ServiceAccountCredentials.from_json_keyfile_name('credentials.json', scope)
 
 client = gspread.authorize(creds)
 
