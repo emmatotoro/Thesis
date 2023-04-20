8b0c517e97342161ed87226a458326a981f5acb0
emmatotoro <130192726+emmatotoro@users.noreply.github.com>
2023-04-19 22:18:23 -0400

    Update Agree.py

--- Agree.py
+++ Agree.py
@@ -212,8 +212,8 @@ with st.empty():
                        ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"
                         ], int(confederate_df.at[0, "pQ10"]) - 1, horizontal=True, key="q10-conf", disabled=True)
                
-              if st.button("Redirect"):
-                   st.redirect("https://wlu.ca1.qualtrics.com/jfe/form/SV_3t0XPINHJ4whqpo")
+            if st.button("Redirect"):
+               st.redirect("https://wlu.ca1.qualtrics.com/jfe/form/SV_3t0XPINHJ4whqpo")
     else:
         st.session_state.page = 0
         
