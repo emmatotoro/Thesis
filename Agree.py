27da32bab6ea36d3d33fa1b6c68ef8d72481c508
emmatotoro <130192726+emmatotoro@users.noreply.github.com>
2023-04-19 22:22:56 -0400

    Update Agree.py

--- Agree.py
+++ Agree.py
@@ -211,9 +211,9 @@ with st.empty():
             conf.radio("Neat, tidy",
                        ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"
                         ], int(confederate_df.at[0, "pQ10"]) - 1, horizontal=True, key="q10-conf", disabled=True)
-               
-            if st.button("Redirect"):
-               st.redirect("https://wlu.ca1.qualtrics.com/jfe/form/SV_3t0XPINHJ4whqpo")
+                
+            link = '(https://wlu.ca1.qualtrics.com/jfe/form/SV_3t0XPINHJ4whqpo)'
+               st.markdown(link, unsafe_allow_html=True)
     else:
         st.session_state.page = 0
         
